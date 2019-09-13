d752a87b3ed69e2e190c1d64fca5f18081a83f8c
jacek9105 <jacek9105@users.noreply.github.com>
2019-09-13 23:52:19 +0200

    ognisko + obrazenia

--- Assets/Skrypt/Ognisko.cs
+++ Assets/Skrypt/Ognisko.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class Ognisko : MonoBehaviour
 {
 
-    public bool czyZapalic;
+    public bool czyPokazac;
     public bool czyMozna;
     public bool czyZapalone;
-    public bool czyPokazac;
+    public bool czyZapalic;
     public ParticleSystem partikle1;
     public ParticleSystem partikle2;
     public AudioSource dzwiek;
     public Light swiatlo;
+    public float timer6;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +28,35 @@ public class Ognisko : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (czyMozna == false)
+            czyZapalic = false;
+
+        if (timer6 >= 60)
+        {
+            timer6 = 0;
+            partikle1.enableEmission = false;
+            partikle2.enableEmission = false;
+            dzwiek.Stop();
+            swiatlo.enabled = false;
+            czyZapalone = false;
+            czyMozna = true;
+            czyZapalic = true;
+            czyPokazac = false;
+        }
+        else if (czyZapalone == true)
+        {
+            timer6 += (Time.deltaTime * 0.9f);
+        }
+
+
     }
     private void OnTriggerStay(Collider col)
     {
          if(col.tag == "Gracz")
         {
+            if (czyZapalone == true)
+                czyPokazac = true;
+            czyZapalic = true;
             if (czyZapalic == true && Input.GetKeyDown(KeyCode.E))
             {
                 partikle1.enableEmission = true;
@@ -42,7 +66,19 @@ public class Ognisko : MonoBehaviour
                 czyZapalone = true;
                 czyMozna = false;
                 czyZapalic = false;
+            }
 
+            if (czyZapalone == true && Input.GetKeyDown(KeyCode.F))
+            {
+                timer6 = 0;
+                partikle1.enableEmission = false;
+                partikle2.enableEmission = false;
+                dzwiek.Stop();
+                swiatlo.enabled = false;
+                czyZapalone = false;
+                czyMozna = true;
+                czyZapalic = true;
+                czyPokazac = false;
             }
         }
     }
@@ -52,7 +88,17 @@ public class Ognisko : MonoBehaviour
         if (czyZapalic == true )
         {
             GUI.Box(new Rect((Screen.width / 2) - 100, (Screen.height * 2) - Screen.height - 150, 200, 50), " Nacisnij E aby zapalic ognisko");
-            GUI.Box(new Rect((Screen.width / 2) - 100, (Screen.height * 2) - Screen.height - 150, 200, 50), " aaaa");
         }
+
+        if (czyPokazac == true)
+        {
+            GUI.Box(new Rect((Screen.width / 2) - 100, (Screen.height * 2) - Screen.height - 150, 200, 50), " Nacisnij F aby zgasic ognisko");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        czyZapalic = false;
+        czyPokazac = false;
     }
 }
