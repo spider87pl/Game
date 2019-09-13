5d76fb97a302d1c80a247646f1306ef1e91570e5
spider87pl <grzspe@st.amu.edu.pl>
2019-09-13 17:41:04 +0200

    Ekwipunek

---
+++ Assets/Skrypt/Ekwipunek.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ekwipunek : MonoBehaviour
+{
+    public int szerokoscOkna;
+    public int wysokoscOkna;
+    
+    void Start()
+    {
+        szerokoscOkna = 60;
+        wysokoscOkna = 60;
+
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect((Screen.width / 2) - szerokoscOkna / 2 + 10, Screen.height * 2 - (wysokoscOkna + 95), szerokoscOkna, wysokoscOkna), ""); 
+    }
+}
