0ad41a683319429c2442f3657e26bf3a02e56acd
jacek9105 <jacek9105@users.noreply.github.com>
2019-09-17 20:54:28 +0200

    Scinanie drzewa
    Dodanie skryptu na scinanie drzewa + niezbedna jest animacja o nazwie animacja

---
+++ Assets/Skrypt/Drzewo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drzewo : MonoBehaviour
+{
+    public int treeHP = 5;
+    public Transform klody;
+    public GameObject drzewo;
+    public Rigidbody rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        drzewo = this.gameObject;
+        rb.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(treeHP <=0)
+        {
+            rb.isKinematic = false;
+            TreeDestroy();
+        }
+    }
+    void TreeDestroy()
+    {
+        Destroy(drzewo);
+        Vector3 pozycja = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+        Instantiate(klody, drzewo.transform.position + new Vector3(0, 0, 0) + pozycja, Quaternion.identity);
+        Instantiate(klody, drzewo.transform.position + new Vector3(2, 2, 2) + pozycja, Quaternion.identity);
+        Instantiate(klody, drzewo.transform.position + new Vector3(4, 4, 4) + pozycja, Quaternion.identity);
+    }
+
+}
---
+++ Assets/Skrypt/DrzewoGracz.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrzewoGracz : MonoBehaviour
+{
+    public bool czyMamSiekiere = false;
+    public bool czyUderzam = false;
+    public bool czyMoznaUderzyc = true;
+    public int czasUderzenia = 1;
+    CharacterController controller; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        czyMamSiekiere = true;
+        controller = GameObject.FindGameObjectWithTag("Gracz").GetComponent < CharacterController >();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(czyMamSiekiere == true && czyMoznaUderzyc == true)
+        {
+            if(Input.GetMouseButton(0))
+            {
+                Debug.Log("animacja");  //wstawic animacje
+                //animation.Play("Atak");
+                czyUderzam = true;
+                czyMoznaUderzyc = false;
+            }
+        }
+        if (czyMoznaUderzyc == false)
+        {
+            czasUderzenia = 0;
+        }
+        if(czasUderzenia <= 0 )
+        {
+            czasUderzenia = 1;
+            czyMoznaUderzyc = true;
+            czyUderzam = false;
+        }
+    }
+}
